153b6ad860a9cfe4b0f56fcec7cff0bd31874a73
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-25 18:46:17 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+ if speed > 60 || speed < 40
+  true
+else speed == 41...60
+  false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
-	
 
 
