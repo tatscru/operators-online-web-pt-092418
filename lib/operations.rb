23d08a1f669f661b2d16ad834b1b047710a90317
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-25 18:41:02 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+ if speed > 60
+  true
+ else speed < 40
+  true 
+# elsif speed == 41...60
+#   false
+#   end 
 end
-
+end
 
 
 def not_safe?(speed)
-	
+  speed < 40 ? "true" :"false"
 end
 	
-
+def not_safe?(speed)
+  speed > 60 ? "true" : "false"
+end
 
